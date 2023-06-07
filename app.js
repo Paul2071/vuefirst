ded92af51adec1c1c72056959f968a2f54b4e6a0
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-06-07 14:38:05 +0100

    atomic commits as I am getting to grips with it all - using v-for to output a list based on array of data in app.js

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const app = Vue.createApp({
   data() {
     return {
-        type: "Contrast",
-        name: "Doomfire Magenta",
-        colour: "Purple",
+      type: "Contrast",
+      name: "Doomfire Magenta",
+      colour: "Purple",
         owned: false,
         collected: "Collected",
+        paints: [
+          { type: "Contrast",
+          name: "Doomfire Magenta",
+          colour: "Purple" },
+          { type: "Contrast",
+          name: "Striking Scorpion Green",
+          colour: "Green" },
+          { type: "Contrast",
+          name: "Flesh tearer red",
+          colour: "Red" }
+        ]
         
 
     };
